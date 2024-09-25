52ee43e4aa6ccb3310caddddc26f3a18d207793c
Nikita Radzevicius <nradzevicius10@gmail.com>
2024-09-25 01:25:01 +0100

    :zap: more testing of discord.py library

--- bot.py
+++ bot.py
@@ -1,29 +1,30 @@
 import discord # Discord Bot library
 from discord import app_commands
 from discord.ext import commands
-import requests # HTTP request library
 from config import botToken
+import requests # HTTP request library
 
 intents = discord.Intents.all()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user: # So it doesn't reply to itself
-        return
-    
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-@bot.tree.command(name="jello")
-async def jello(interaction: discord.Interaction):
+@bot.tree.command(name="hello")
+async def hello(interaction: discord.Interaction):
     await interaction.response.send_message(f"Hey {interaction.user.mention}! This is a slash command!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {round(bot.latency * 1000)}ms")
 
 bot.run(botToken)
